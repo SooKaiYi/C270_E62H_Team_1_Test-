bf43509447c44c77efbe2b390a7a19eeee336462
noobpalyer <25049440@myrp.edu.sg>
2026-07-14 19:13:16 +0800

    added profile

---
+++ backend/routes/profileRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+const rewardsController = require("../controllers/rewardsController");
+
+function requireLogin(req, res, next) {
+    if (!req.session.user) {
+        return res.redirect("/login.html");
+    }
+    next();
+}
+
+router.get("/profile", requireLogin, (req, res) => {
+    const user = req.session.user;
+    const record = rewardsController.getOrCreateUserRewards(user);
+
+    res.render("profile", { user, rewards: record });
+});
+
+module.exports = router;
